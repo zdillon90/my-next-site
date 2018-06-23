019f33f2f737d181f82749983ea9aacb6d0eefe4
Zach Dillon <zach@shapeways.com>
2018-06-22 23:26:48 -0400

    trying a new button with passed props

---
+++ components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+`;
+
+export default () => <Button>{this.props.value}</Button>;
